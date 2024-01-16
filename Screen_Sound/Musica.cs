4eecfd9fe0f425d29d1cd0dc1b2f7421630c3ca0
lele-rdm <leandromartinspinto2004@gmail.com>
2024-01-16 11:50:11 -0300

    Implementando métodos na classe Musica

--- Screen_Sound/Musica.cs
+++ Screen_Sound/Musica.cs
@@ -1,8 +1,23 @@
 ﻿
 class Musica
 {
-    string nome;
-    string artista;
-    int duracao;
-    bool disponivel;
+    public string nome;
+    public string artista;
+    public int duracao;
+    public bool disponivel;
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome da Música: {nome}");
+        Console.WriteLine($"Nome do Artista: {artista}");
+        Console.WriteLine($"Tempo de Duração: {duracao}");
+        if (disponivel) 
+        {
+            Console.WriteLine("Disponibilidade: Disponível");
+        }
+        else
+        {
+            Console.WriteLine("Disponibilidade: Indisponível\nPara ter acesso adquira o Plano Plus.");
+        }
+    }
 }
