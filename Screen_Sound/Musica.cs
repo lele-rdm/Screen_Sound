5fafc6b2c179bc85367516153d39ad69d2ec6703
lele-rdm <leandromartinspinto2004@gmail.com>
2024-01-18 10:18:49 -0300

    Criando a classe Album e a liste de músicas

--- Screen_Sound/Musica.cs
+++ Screen_Sound/Musica.cs
@@ -5,13 +5,7 @@ class Musica
     public string Artista { get; set; }
     public int Duracao { get; set; }
     public bool Disponivel { get; set; }
-    public string DescricaoResumida 
-    {
-        get
-        {
-            return $"A música {Nome} pertence á banda {Artista}";
-        }
-    }
+    public string DescricaoResumida => $"A música {Nome} pertence á banda {Artista}";
     public void ExibirFichaTecnica()
     {
         Console.WriteLine($"Nome da Música: {Nome}");
