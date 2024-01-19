aa61be5d52a48f67d224f5e2f4e4bf008d677efa
lele-rdm <leandromartinspinto2004@gmail.com>
2024-01-19 10:45:16 -0300

    Relacionamento entre as classes e construtores

--- Screen_Sound/Album.cs
+++ Screen_Sound/Album.cs
@@ -1,1 +1,19 @@
 ﻿
+class Album
+{
+    private List<Musica> musicas = new List<Musica>();
+    public string Nome { get; set; }
+    public int DuracaoTotal { get; set; }
+    public void AdicionarMusica(Musica musica)
+    {
+        musicas.Add(musica);
+    }
+    public void ExibirMusicasDoAlbum()
+    {
+        Console.WriteLine($"Lista de Músicas do Álbum {Nome}:\n");
+        foreach (var musica in musicas)
+        {
+            Console.WriteLine($"Música: {musica.Nome}");
+        }
+    }
+}
--- Screen_Sound/Program.cs
+++ Screen_Sound/Program.cs
@@ -1,16 +1,17 @@
 ﻿
+Album albumDoQueen =  new Album();
+albumDoQueen.Nome = "A night at the opera";
+
 Musica musica1 = new Musica();
-musica1.Nome = "Roxane";
-musica1.Artista = "The Police";
-musica1.Duracao = 273;
-musica1.Disponivel = true;
-Console.WriteLine(musica1.Disponivel);
+musica1.Nome = "Love of my life";
 
 Musica musica2 = new Musica();
-musica2.Nome = "Vertigo";
-musica2.Artista = "U2";
-musica2.Duracao = 367;
-musica2.Disponivel = false;
+musica2.Nome = "Bohemian Rhapsody";
 
-musica1.ExibirFichaTecnica();
-musica2.ExibirFichaTecnica();
+albumDoQueen.AdicionarMusica(musica1);
+albumDoQueen.AdicionarMusica(musica2);
+
+Banda queen = new Banda();
+queen.Nome = "Queen";
+queen.AdicionarAlbum(albumDoQueen);
+queen.ExibirDiscografia();
